0707f8df17b218eb63f679bb1243b8f2409b891c
ishiahirake <ishiahirake@gmail.com>
2019-08-09 23:51:08 +0900

    Add finish and cancel.

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -4,6 +4,8 @@ import logo from './logo.svg';
 
 function Todo() {
   const [todos, setTodos] = React.useState([])
+  const [finishedCount, setFinishedCount] = React.useState(0)
+  const [cancelledCount, setCancelledCount] = React.useState(0)
 
   function handleAddTodo(event) {
     const todoInput = document.getElementById('todo-input')
@@ -11,13 +13,34 @@ function Todo() {
     todoInput.value = ''
   }
 
+  function handleFinishTodo(todo, index) {
+    setFinishedCount(finishedCount + 1)
+    todos.splice(index, 1)
+    setTodos([...todos])
+  }
+
+  function handleCancelTodo(todo, index) {
+    setCancelledCount(cancelledCount + 1)
+    todos.splice(index, 1)
+    setTodos([...todos])
+  }
+
   return (
     <div>
+      <div>
+        <span>已完成 {finishedCount} 件</span>
+        <span> / </span>
+        <span>已取消 {cancelledCount} 件</span>
+      </div>
       <ul>
         {todos.map((todo, index) => {
           return (
             <li key={index}>
               <span>{todo}</span>
+              <span>
+                <button onClick={() => handleFinishTodo(todo, index)}>完成</button>
+                <button onClick={() => handleCancelTodo(todo, index)}>取消</button>
+              </span>
             </li>
           )
         })}
